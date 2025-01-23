302878e050e0f5a72acf7f3a4f6397cc4a6967b4
rohnshrma <rohn11shrma@gmail.com>
2025-01-23 12:18:43 +0530

    Add Timer component and update styling for improved UI

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Book from "./Book";
 import books from "./data.js";
 import Counter from "./Counter.js";
-
+import Timer from "./Timer.js";
 function App() {
   console.log("App Running");
 
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
+      <Timer />
     </div>
   );
 }
---
+++ src/Timer.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Timer = function () {
+  const [timer, setTimer] = useState("Get Time...");
+
+  const clickHandler = () => {
+    setTimer(new Date().toLocaleTimeString());
+  };
+
+  return (
+    <div className="timer">
+      <h2>{timer}</h2>
+      <button onClick={clickHandler}>Set Time</button>
+    </div>
+  );
+};
+
+export default Timer;
