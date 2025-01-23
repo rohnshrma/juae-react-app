f90b40f74aeda73211eca0e87d4e6e2bfbf22fa1
rohnshrma <rohn11shrma@gmail.com>
2025-01-23 12:25:03 +0530

    Refactor Timer component to automatically update time every second and remove manual button

--- src/Timer.js
+++ src/Timer.js
@@ -1,3 +1,22 @@
+// import React, { useState } from "react";
+
+// const Timer = function () {
+//   const [timer, setTimer] = useState("Get Time...");
+
+//   const clickHandler = () => {
+//     setTimer(new Date().toLocaleTimeString());
+//   };
+
+//   return (
+//     <div className="timer">
+//       <h2>{timer}</h2>
+//       <button onClick={clickHandler}>Set Time</button>
+//     </div>
+//   );
+// };
+
+// export default Timer;
+
 import React, { useState } from "react";
 
 const Timer = function () {
@@ -7,10 +26,12 @@ const Timer = function () {
     setTimer(new Date().toLocaleTimeString());
   };
 
+  setInterval(clickHandler, 1000);
+
   return (
     <div className="timer">
       <h2>{timer}</h2>
-      <button onClick={clickHandler}>Set Time</button>
+      {/* <button onClick={clickHandler}>Set Time</button> */}
     </div>
   );
 };
